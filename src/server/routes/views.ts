05862aa6a608131a0653e3068fc7aac80fe7d776
tgnlex <alexanderwebber76@gmail.com>
2026-04-17 00:45:30 -0400

    init

--- src/server/routes/views.ts
+++ src/server/routes/views.ts
@@ -3,11 +3,11 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('index.html')
+  res.render('pages/index.html')
 });
 
 router.get('/blog', (req, res) => {
-  res.render('blog.html')
+  res.render('pages/blog.html')
 });
 
 export default router;
