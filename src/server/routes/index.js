817a2aab37ef63fd45660221d293910023a81007
tgnlex <alexanderwebber76@gmail.com>
2026-05-09 11:14:33 -0400

    auto-commit

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -10,4 +10,11 @@ router.get('/blog', (req, res) => {
   res.render('pages/blog')
 })
 
+router.get('/blog/:id', async (req, res) => {
+  const id = req.params.id;
+  const response = await fetch(`http://localhost:4000/api/posts/${id}`);
+  const post = await response.json();
+  res.render('pages/post', {post: post});
+})
+
 export default router;
